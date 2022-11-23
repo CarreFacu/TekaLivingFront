17e629d99fb25fa4bb32490cf55160741721eb0c
CarreFacu <facundo.carre@hotmail.com>
2022-11-23 10:59:40 -0300

    se agrego el fondo

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import './Login.css'
 import {Grid, Button} from '@mui/material';
 import TextField from "@mui/material/TextField";
+import fondo from '../../../src/assets/images/mueble1.jpg'
 const Login = () => {
     const [inputs, setInputs ] = useState({
         email: '',
@@ -19,19 +20,21 @@ const Login = () => {
         console.log(inputs)
     }
     return (
-        <form onSubmit={HandleSubmit}>
-            <Grid className='fondo'>
-                <Grid item md={12} className='inputs'>
-                    <TextField id="outlined-basic" label="Email" variant="outlined" onChange={handleChange} value={inputs.email || ' '} name='email'/>
+        <div className='background-login'>
+            <form onSubmit={HandleSubmit}>
+                <Grid className='fondo'>
+                    <Grid item md={12} className='inputs'>
+                        <TextField id="outlined-basic" label="Email" variant="outlined" onChange={handleChange} value={inputs.email || ' '} name='email'/>
+                    </Grid>
+                    <Grid item md={12} className='inputs' >
+                        <TextField className='inputs' type="password" id="outlined-basic" label="Contraseña" variant="outlined" onChange={handleChange} value={inputs.contraseña || ' '} name='contraseña'/>
+                    </Grid>
+                    <Grid item md={12} className="inputs">
+                        <Button type='submit' variant="contained">Log In</Button>
+                    </Grid>
                 </Grid>
-                <Grid item md={12} className='inputs' >
-                    <TextField className='inputs' type="password" id="outlined-basic" label="Contraseña" variant="outlined" onChange={handleChange} value={inputs.contraseña || ' '} name='contraseña'/>
-                </Grid>
-                <Grid item md={12} className="inputs">
-                    <Button type='submit' variant="contained">Log In</Button>
-                </Grid>
-            </Grid>
-        </form>
+            </form>
+        </div>
      );
 }
  
